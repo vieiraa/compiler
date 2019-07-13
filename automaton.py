182e25a17730f8e2a5ec6731b2c79ca733185fd6
Jordy Vieira <jordyvieira@hotmail.com>
2019-07-13 00:37:54 -0300

    Implement comments and errors, and fix bugs

--- automaton.py
+++ automaton.py
@@ -2,25 +2,27 @@ KEYWORDS = ['program', 'var', 'integer', 'real',
             'boolean', 'procedure', 'begin', 'end',
             'if', 'then', 'else', 'while', 'do', 'not']
 
+aux_c = ''
+row = 1
+column = 1
+
 class Token:
-    def __init__(self, tok, what, line, column):
+    def __init__(self, tok, what, row, col):
         self.token = tok
-        self.line = line
-        self.column = column
+        self.row = row
+        self.col = col
         self.what = what
 
     def __repr__(self):
-        return f"<{self.line}:{self.column} | {self.token} | {self.what}>\n"
+        return f"<{self.row}:{self.col} | {self.token} | {self.what}>\n"
 
     def __str__(self):
-        return f"<{self.line}:{self.column} | {self.token} | {self.what}>\n"
+        return f"<{self.row}:{self.col} | {self.token} | {self.what}>\n"
 
 class State:
     def __init__(self, name, accepting):
         self.name = name
         self.accepting = accepting
-        self.line = 0
-        self.column = 0
 
     def read(self, char, next_char):
         pass
@@ -31,83 +33,103 @@ class StartState(State):
         super().__init__('Start', False)
 
     def read(self, char, next_char):
-        if char.isspace():
-            self.column += 1
-            return self
+        global row
+        global column
 
         if char == '\n':
-            self.line += 1
-            self.column = 0
-            return self
+            row += 1
+            column = 1
+            return '', self
 
-        if char.isnumeric():
-            return IntegerState()
+        if char.isspace():
+            column += 1
+            return '', self
+        
+        if char == '{':
+            return '', CommentState(row, column)
+
+        if char == '\n' or char.isspace():
+            return '', self
+
+        if char.isnumeric() and (next_char.isnumeric() or next_char == '.'):
+            return char, IntegerState()
+
+        if char.isnumeric() and not (next_char.isnumeric() or next_char == '.'):
+            return char, TokenFoundState('Integer')
 
         if char in ['(', ')', ',', '.', ';']:
-            return TokenFoundState('Delimiter')
+            return char, TokenFoundState('Delimiter')
 
         if char == ':':
             if next_char == '=':
-                return DelimiterState()
-            return TokenFoundState('Delimiter')
+                return char, DelimiterState()
+            return char, TokenFoundState('Delimiter')
 
         if char in ['+', '-']:
-            return TokenFoundState('Aditive')
+            return char, TokenFoundState('Aditive')
 
         if char in ['*', '/']:
-            return TokenFoundState('Multiplicative')
+            return char, TokenFoundState('Multiplicative')
 
-        if char.isalpha() or char == '_':
-            return IdentifierState()
-
+        if (char.isalpha() or char == '_') and (next_char.isalnum() or next_char == '_'):
+            return char, IdentifierState()
+        
+        if (char.isalpha() or char == '_') and not (next_char.isalnum() or next_char == '_'):
+            return char, TokenFoundState('Identifier')
+        
         if char in ['=', '<', '>']:
-            return RelationalState(char)
+            return char, RelationalState(char)
 
 class IntegerState(State):
     def __init__(self):
         super().__init__('Integer', False)
 
     def read(self, char, next_char):
-        if char == '.' or next_char == '.':
-            return RealState()
+        if char == '.' and next_char.isnumeric():
+            return char, RealState()
 
-        if next_char.isnumeric():
-            return self
+        if char == '.' and not next_char.isnumeric():
+            return char, TokenFoundState('Real')
 
-        if not next_char.isnumeric() or next_char != '.':
-            return TokenFoundState(self.name)
+        if char.isnumeric() and next_char.isnumeric():
+            return char, self
+
+        if char.isnumeric() and not next_char.isnumeric():
+            return char, TokenFoundState(self.name)
 
 class RealState(State):
     def __init__(self):
         super().__init__('Real', False)
 
     def read(self, char, next_char):
-        if char in ['e', 'E'] or next_char in ['e', 'E']:
-            return PowerState1()
+        global aux_c
+        if char in ['e', 'E'] and aux_c.isnumeric():
+            return char, PowerState1()
 
-        if next_char.isnumeric():
-            return self
-
-        return TokenFoundState(self.name)
+        if char.isnumeric() and next_char.isnumeric():
+            return char, self
+        
+        if char.isnumeric() and not next_char.isnumeric():
+            return char, TokenFoundState(self.name)
 
 class PowerState1(State):
     def __init__(self):
         super().__init__('Power', False)
 
     def read(self, char, next_char):
-        if char in ['+', '-'] or next_char in ['+', '-']:
+        if char in ['+', '-']:
             self.numbers = True
-            return PowerState2()
+            return char, PowerState2()
 
 class PowerState2(State):
     def __init__(self):
         super().__init__('Power', False)
 
     def read(self, char, next_char):
-        if next_char.isnumeric():
-            return self
-
-        return TokenFoundState('Real')
+        if char.isnumeric() and next_char.isnumeric():
+            return char, self
+        if char.isnumeric() and not next_char.isnumeric():
+            return char, TokenFoundState('Real')
 
 class IdentifierState(State):
     def __init__(self):
@@ -115,9 +137,9 @@ class IdentifierState(State):
 
     def read(self, char, next_char):
         if char.isalnum() and (next_char.isalnum() or next_char == '_'):
-            return self
+            return char, self
 
-        return TokenFoundState(self.name)
+        return char, TokenFoundState(self.name)
 
 class DelimiterState(State):
     def __init__(self):
@@ -125,9 +147,9 @@ class DelimiterState(State):
 
     def read(self, char, next_char):
         if char == '=':
-            return TokenFoundState('Assignment')
+            return char, TokenFoundState('Assignment')
 
-        return TokenFoundState(self.name)
+        return '', TokenFoundState(self.name)
 
 class RelationalState(State):
     def __init__(self, what):
@@ -137,15 +159,34 @@ class RelationalState(State):
     def read(self, char, next_char):
         if self.what == '<':
             if next_char in ['=', '<', '>']:
-                return self
-            return TokenFoundState(self.name)
+                return char, self
+            return char, TokenFoundState(self.name)
 
         if self.what == '>':
             if next_char == '=':
-                return self
-            return TokenFoundState(self.name)
+                return char, self
+            return char, TokenFoundState(self.name)
 
-        return TokenFoundState(self.name)
+        return '', TokenFoundState(self.name)
+
+class CommentState(State):
+    def __init__(self, row, col):
+        super().__init__('Comment', False)
+        self.s_row = row
+        self.s_col = col
+
+    def read(self, char, next_char):
+        global row, column
+        if char != '}':
+            if char.isspace():
+                column += 1
+            elif char == '\n':
+                row += 1
+                column = 1
+            return '', self
+        self.s_row = 0
+        self.s_col = 0
+        return StartState()
 
 class TokenFoundState(State):
     def __init__(self, name):
@@ -159,40 +200,56 @@ class Automaton:
         state = self.start
         temp = ''
         tokens = []
-        line = 0
-        column = 0
         what = ''
-        for i in range(len(string)):
-            c = string[i]
+        global row, column
+        for i, c in enumerate(string):
+            try:
+                if c == '}' and state.name == 'Comment':
+                    state = self.start
+                    continue
+                
+                if i == len(string) - 1:
+                    aux = string[i]
+                else:
+                    aux = string[i + 1]
 
-            if c == '\n':
-                line += 1
-                column = 0
-                continue
+                global aux_c
+                if i == len(string) - 2:
+                    aux_c = string[i + 1]
+                elif i == len(string) - 1:
+                    aux_c = string[i]
+                else:
+                    aux_c = string[i + 2]
 
-            temp += c
-            if i == len(string) - 1:
-                aux = string[i]
-            else:
-                aux = string[i + 1]
+                char, state = state.read(c, aux)
+                temp += char
+                if row == 10:
+                    print(temp)
 
-            state = state.read(c, aux)
-            if state.accepting:
-                what = state.name
-                if temp in KEYWORDS:
-                    what = 'Keyword'
+                if state.accepting:
+                    temp = temp.strip()
+                    what = state.name
+                    if temp in KEYWORDS:
+                        what = 'Keyword'
 
-                elif temp == 'or':
-                    what = 'Aditive'
+                    elif temp == 'or':
+                        what = 'Aditive'
 
-                elif temp == 'and':
-                    what = 'Multiplicative'
+                    elif temp == 'and':
+                        what = 'Multiplicative'
 
-                tokens.append(Token(temp.strip(), what, line, column))
+                    tokens.append(Token(temp.strip(), what, row, column))
 
-                column += len(temp)
+                    column += len(temp)
 
-                temp = ''
-                state = self.start
+                    temp = ''
+                    state = self.start
+            except:
+                print('Invalid symbol: ', c)
+                exit(0)
+
+        if state.name == 'Comment':
+            print(f'Not closed comment starting at {state.s_row}:{state.s_col}')
+
 
         return tokens
--- test.pas
+++ test.pas
@@ -1,14 +1,18 @@
+{comment}
 program teste;
 var
+{comment}
     valor1: integer;
     valor2: real;
 begin
-    valor1:=10;
-    valor2 := 20;
-    valor2 := 2.1e+12;
+    valor1:=1012;
+   valor2 := 2.;
+   valor2 := .10e+20;
+   1e
     if () then
     end
     30 <> 50;
     50 < 10;
     20 >= 31;
 end.
+
