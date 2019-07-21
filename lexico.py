3ba293e16460d855a93b24a4a471ee4d03de934b
Jordy Vieira <jordyvieira@hotmail.com>
2019-07-21 16:02:47 -0300

    Reimplement as regex

--- lexico.py
+++ lexico.py
@@ -1,16 +1,77 @@
 import sys
-from automaton import Automaton
+import re
+
+KEYWORDS = '^(program|var|integer|real|boolean|procedure|begin|end|if|then|else|while|do|not)\\b'
+IDENTIFIER = '^[a-z]\w*\\b'
+INTEGER = '^\d+(?!\.)'
+REAL = '^\d+\.\d*(e(\+|-)\d*)?'
+DELIMITER = '^(\.|,|;|:(?!=)|\(|\))'
+ASSIGNMENT = '^:='
+RELATIONAL = '^((?<!:)=|<(?!(=|>))|>(?!=)|<=|>=|<>)'
+ADITIVE = '^(\+|-|or)'
+MULTIPLICATIVE = '^(\*|/|and)'
+COMMENT = '{.*?}'
+
+PATTERNS = {
+    'Keyword': KEYWORDS,
+    'Identifier': IDENTIFIER,
+    
+    'Real': REAL,
+    'Integer': INTEGER,
+    'Delimiter': DELIMITER,
+    'Assignment': ASSIGNMENT,
+    'Relational': RELATIONAL,
+    'Aditive': ADITIVE,
+    'Multiplicative': MULTIPLICATIVE,
+    'Comment': COMMENT
+}
+
+class Token:
+    def __init__(self, tok, what, row):
+        self.token = tok
+        self.row = row
+        self.what = what
+
+    def __repr__(self):
+        return f"<{self.row} | {self.token} | {self.what}>\n"
+
+    def __str__(self):
+        return f"<{self.row} | {self.token} | {self.what}>\n"
+
+def tokenize(code):
+    code = re.sub(PATTERNS['Comment'], '', code, flags=re.DOTALL)
+    code = [line.strip() for line in code.split('\n') if line != '']
+    tokens = []
+
+    for i, line in enumerate(code):
+        j = 0
+        while j < len(line):
+            j += next_col(line[j], line[j:], i, j, tokens)
+
+    return tokens
+
+def next_col(char, line, row, col, tokens):
+    if re.match(r'\s', char):
+        return 1
+
+    if re.match('{', char):
+        raise Exception('Unclosed comment')
+
+    for key, value in PATTERNS.items():
+        result = re.match(value, line, flags=re.IGNORECASE)
+
+        if result:
+            tokens.append(Token(result.group(0), key, row))
+            return result.span()[1]
+
+    raise Exception(f'Invalid symbol: {line}')
 
 if __name__ == '__main__':
-    tokens = []
     if len(sys.argv) != 2:
         print(f'Usage: python lexico.py filename')
         sys.exit(0)
 
     with open(sys.argv[1], 'r') as infile:
-        auto = Automaton()
-        tokens = auto.tokenize(infile.read())
-
-    with open('table.txt', 'w') as outfile:
-        for token in tokens:
-            outfile.write(str(token))
+        code = infile.read()
+        tokens = tokenize(code)
+        print(tokens)
